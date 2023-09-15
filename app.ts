e150a36736f276392cbca4f2846880b0992662bf
racoonpapa <racoonpapa@naver.com>
2023-09-16 02:40:29 +0900

    feat: initial setup

---
+++ app.ts
@@ -0,0 +1,119 @@
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from "fs";
+
+enum Priority {
+    None,
+    Low,
+    Medium,
+    High
+}
+
+class TodoItem {
+    id: string = ""
+    content: string = ""
+    priority: Priority = Priority.None
+    dueDate: Date | null = null
+    done: boolean = false
+
+    private static priorityStringMap: {[key in Priority]?: string} = {
+        [Priority.None]: "None",
+        [Priority.Low]: "Low",
+        [Priority.Medium]: "Medium",
+        [Priority.High]: "High"
+    }
+
+    getPriorityString(): string {
+        return TodoItem.priorityStringMap[this.priority] || ""
+    }
+
+    constructor(data: string | {id: string, content: string, priority: number, due_date: Date | null, done: boolean}) {
+        if(typeof data === 'string') {
+            this.id = uuidv4()
+            this.content = data
+        } else {
+            this.id = data.id
+            this.content = data.content
+            this.priority = data.priority
+            this.dueDate = data.due_date
+            this.done = data.done
+        }
+    }
+
+    withPriority(priority: Priority): TodoItem {
+        this.priority = priority
+        return this
+    }
+
+    withDueDate(dueDate: Date | null): TodoItem {
+        this.dueDate = dueDate
+        return this
+    }
+
+    toString(): string {
+        let out = `[${this.done ? 'x': ' '}] ${this.content}`
+        if(this.priority != Priority.None) {
+            out = out + ` [${this.getPriorityString()}]`
+        }
+        if(this.dueDate != null) {
+            out = out + ` (${this.dueDate})`
+        }
+        return out
+    }
+}
+
+class TodoList {
+    items: Array<TodoItem> = new Array<TodoItem>()
+
+    toString(): string {
+        const arr: Array<string> = []
+        this.items.forEach(item => {
+            arr.push(item.toString())
+        })
+        return arr.join("\n")
+    }
+
+    static fromJson(jsonData: string): TodoList | null {
+        const obj = JSON.parse(jsonData)
+
+        if(Array.isArray(obj)) {
+            const list = new TodoList()
+            for(const data of obj) {
+                const item = new TodoItem(data)
+                list.items.push(item)
+            }
+            return list
+        }
+        return null
+    }
+
+    toJson(): string {
+        return JSON.stringify(this.items)
+    }
+}
+
+
+try {
+    let started: number, ended: number
+
+    const data = fs.readFileSync("todo.json", "utf-8")
+    started = performance.now()
+    const items = TodoList.fromJson(data)
+    ended = performance.now()
+
+    const elapsedFrom = (ended - started) * 0.001
+
+    if(items !== null) {
+        console.log(items.toString())
+
+        started = performance.now()
+        items.toJson();
+        ended = performance.now()
+
+        const elapsedTo = (ended - started) * 0.001
+
+        console.log(`fromJson - ${elapsedFrom} seconds, toJson - ${elapsedTo} seconds`)
+    }
+} catch(e) {
+    console.error("an error occurred: ", e)
+}
+
