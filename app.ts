46c25a945ac8554e6f8a4b976341321a048b5139
racoonpapa <racoonpapa@naver.com>
2023-09-16 15:27:13 +0900

    feat: convert the container of TodoList with Map instead of Array

--- app.ts
+++ app.ts
@@ -8,23 +8,25 @@ enum Priority {
     High
 }
 
-class TodoItem {
-    id: string = ""
-    content: string = ""
-    priority: Priority = Priority.None
-    dueDate: Date | null = null
-    done: boolean = false
-
-    private static priorityStringMap: {[key in Priority]?: string} = {
+class Priorities {
+    private static lookup: {[key in Priority]?: string} = {
         [Priority.None]: "None",
         [Priority.Low]: "Low",
         [Priority.Medium]: "Medium",
         [Priority.High]: "High"
     }
 
-    getPriorityString(): string {
-        return TodoItem.priorityStringMap[this.priority] || ""
+    public static toString(priority: Priority): string {
+        return Priorities.lookup[priority] || ""
     }
+}
+
+class TodoItem {
+    id: string = ""
+    content: string = ""
+    priority: Priority = Priority.None
+    dueDate: Date | null = null
+    done: boolean = false
 
     constructor(data: string | {id: string, content: string, priority: number, due_date: Date | null, done: boolean}) {
         if(typeof data === 'string') {
@@ -52,7 +54,7 @@ class TodoItem {
     toString(): string {
         let out = `[${this.done ? 'x': ' '}] ${this.content}`
         if(this.priority != Priority.None) {
-            out = out + ` [${this.getPriorityString()}]`
+            out = out + ` [${Priorities.toString(this.priority)}]`
         }
         if(this.dueDate != null) {
             out = out + ` (${this.dueDate})`
@@ -61,8 +63,9 @@ class TodoItem {
     }
 }
 
+
 class TodoList {
-    items: Array<TodoItem> = new Array<TodoItem>()
+    items: Map<string, TodoItem> = new Map();
 
     toString(): string {
         const arr: Array<string> = []
@@ -79,7 +82,7 @@ class TodoList {
             const list = new TodoList()
             for(const data of obj) {
                 const item = new TodoItem(data)
-                list.items.push(item)
+                list.items.set(item.id, item)
             }
             return list
         }
@@ -87,7 +90,8 @@ class TodoList {
     }
 
     toJson(): string {
-        return JSON.stringify(this.items)
+        // 가장 간단한 방법, 다만 Array를 거쳐가기 때문에 메모리 사용량이 매우 많아진다.
+        return JSON.stringify(Array.from(this.items.values()))
     }
 }
 
@@ -96,19 +100,18 @@ try {
     let started: number, ended: number
 
     const data = fs.readFileSync("todo.json", "utf-8")
+
     started = performance.now()
     const items = TodoList.fromJson(data)
     ended = performance.now()
-
     const elapsedFrom = (ended - started) * 0.001
 
     if(items !== null) {
         console.log(items.toString())
 
         started = performance.now()
-        items.toJson();
+        const newJson= items.toJson()
         ended = performance.now()
-
         const elapsedTo = (ended - started) * 0.001
 
         console.log(`fromJson - ${elapsedFrom} seconds, toJson - ${elapsedTo} seconds`)
